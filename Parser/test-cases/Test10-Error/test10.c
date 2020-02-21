d278708afb8a779d1535b171d99d618ec8518016
chandannaik999 <chandannaik999@gmail.com>
2020-02-21 19:14:10 +0530

    updation of test-case 9,10 in parser

--- Parser/test-cases/Test10-Error/test10.c
+++ Parser/test-cases/Test10-Error/test10.c
@@ -1,1 +1,16 @@
-#include stdio.h>
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    char ch;//Character Datatype
+
+    for (i=0;i<n;i++){
+        if(i<10){
+            int x;
+            while(x<10){
+                x++;
+        }
+
+    }
+    
+}
--- Parser/test-cases/Test9-Error/test9.c
+++ Parser/test-cases/Test9-Error/test9.c
@@ -1,5 +1,15 @@
-#include <stdio.h>
-int main()
-{
-	int 9abi = 10;
+#include<stdio.h>
+
+int fun(char x){
+	return x*x;
+}
+
+void main(){
+	int a=2,b,c,d,e,f,g,h;
+
+	c=a+b;
+	d=a*b
+	h=fun(b);
+
+	//This Test case no semicolon
 }
